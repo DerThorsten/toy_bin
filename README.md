37a07697f0c718d4dbba254bffe298112fd76908
DerThorsten <derthorstenbeier@gmail.com>
2022-02-15 12:41:53 +0100

    added files

---
+++ README.md
@@ -0,0 +1,2 @@
+# toy_bin
+toy binary for experiments with emscripten in conjunction conda-forgen
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <toy_lib/toy_lib.hpp>
+
+
+
+int main()
+{
+    toy_lib::hello_world();
+}
